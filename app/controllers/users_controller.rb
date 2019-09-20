ea99e258d73f892beb22b6a84931e3a6d9773a86
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-09-20 20:15:37 +0100

    review validator

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
         @user.save
         redirect_to user_path(@user)
     else
-        redirect_to new_user_path
+        render 'users/new'
     end
     end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,15 @@ class Review < ApplicationRecord
     
     belongs_to :user
     belongs_to :restaurant
+    validate :user_cannot_review_the_same_restaurant_twice
+
+    def user_cannot_review_the_same_restaurant_twice
+        restaurant = self.restaurant_id
+        self.user_id.restaurants.each do |r|
+            if r.id == restaurant.id
+                errors.add(:user_id, "you can't review the same hotel twice")
+            end
+        end
+    end
 
 end
