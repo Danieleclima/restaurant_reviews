07682cc6cc00a27f60b2a9545e0fc721d569290c
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-09-21 12:55:43 +0100

    rendering restaurant page after invalid review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+
+    def create
+        #binding.pry
+        @review = Review.new(review_params)
+        if @review.valid?
+            @review.save
+            redirect_to user_path(current_user)
+        else
+           @restaurant = Restaurant.find_by(:id => params[:review][:restaurant_id])
+            render 'restaurants/show'
+        end
+    end
+
+    def review_params
+        params.require(:review).permit(:rating, :description, :user_id, :restaurant_id)
+    end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,10 +3,12 @@ class Review < ApplicationRecord
     belongs_to :user
     belongs_to :restaurant
     validate :user_cannot_review_the_same_restaurant_twice
+    validates :rating, presence: true 
 
     def user_cannot_review_the_same_restaurant_twice
         restaurant = self.restaurant_id
-        self.user_id.restaurants.each do |r|
+        user = User.find_by(:id => self.user_id)
+            user.restaurants.each do |r|
             if r.id == restaurant.id
                 errors.add(:user_id, "you can't review the same hotel twice")
             end
